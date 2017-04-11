b6d23422d8afc16934006a9addd0cf0778fd0df8
Lucas Silva <lucasre.silva@hotmail.com>
2017-04-11 17:10:09 -0300

    Base da aplicação

---
+++ src/main/java/io/github/lucasresilva/clichatrabbitmq/ChatCli.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.lucasresilva.clichatrabbitmq;
+
+/**
+ *
+ * @author lucas
+ */
+public class ChatCli {
+    
+    static public void main(String args[]){
+        System.out.println("io.github.lucasresilva.clichatrabbitmq.ChatCli.main()");
+    }
+    
+}
